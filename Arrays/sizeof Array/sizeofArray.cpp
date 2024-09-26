b5c065e3f8913162e089ae2f0e6ca359f56fae9d
QAISER ALI <qaisers.official@gmail.com>
2024-09-26 15:13:32 +0500

    In C++, we do not have the length function as in Java to find array size but we can calculate the size of an array using sizeof() operator trick. First, i found the size occupied by the whole array in the memory and then divide it by the size of the type of element stored in the array. This gave me the number of elements stored in the array.

--- Arrays/sizeof Array/sizeofArray.cpp
+++ Arrays/sizeof Array/sizeofArray.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
-     
+     int array[] = { 191, 500, 600, 400, 150 };
+     // The size of the array[0]
+     cout << "The size of one element in array is: " << sizeof(array[0]) << endl;
+     // The size of the array
+     cout << "The size of the array is: " << sizeof(array) << endl;
+     // length of the array
+     int lengthOfArray = sizeof(array) / sizeof(array[0]);
+        cout << "The length of the array is: " << lengthOfArray << endl;
 return 0;
 }
