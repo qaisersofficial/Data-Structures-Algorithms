2641c717cb7cc0195c56e70e649286d9282c3795
QAISER ALI <qaisers.official@gmail.com>
2024-09-26 12:04:40 +0500

    Refactor array initialization and add array declaration using for loop

--- Arrays/All About Arrays.cpp
+++ Arrays/All About Arrays.cpp
@@ -9,7 +9,17 @@ int main() {
     // int arr[5] = {1, 2, 3, 4, 5};
 
     // Array Initialization in C++ (Without Specifying Size)
-    int arr[] = {1, 2, 3, 4, 5}; //the length of an array is equal to the number of elements inside curly braces.
-    cout << arr[0] << endl;
+    // int arr[] = {1, 2, 3, 4, 5}; //the length of an array is equal to the number of elements inside curly braces.
+    // cout << arr[0] << endl;
+
+
+    // initlize array after decaration (using loop)
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        arr[i] = i + 1; // we can also take input from the user using cin >> arr[i];
+    }// this method is used when we want to take input from the user or we cant to asssign elements one by one to each index of the array.
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " "; // output: 1 2 3 4 5
+    }
 return 0;
 }
