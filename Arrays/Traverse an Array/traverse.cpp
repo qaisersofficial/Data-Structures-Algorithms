f260657c5cafa4346d9d5cc96580becdf1d18c58
QAISER ALI <qaisers.official@gmail.com>
2024-09-26 13:06:36 +0500

    Traverse the array elements

--- Arrays/Traverse an Array/traverse.cpp
+++ Arrays/Traverse an Array/traverse.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() {
+     //initialize an Array strings
+     string students_name [5]= { "Qaiser", "Ali", "Sharif", "Haider", "Soban" }; 
+      // Traverse the array using for loop
+      cout << "{ ";
+     for (int i = 0; i < 5 ; i++)
+     {
+        // Print the array elements using indexing
+        cout << "\"" << students_name[i] << "\"";
+        if (i < 4) {
+            cout << ", ";
+        }
+     }
+     cout << " }";
      
 return 0;
 }
