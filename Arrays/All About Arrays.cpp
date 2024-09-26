48ec6d326610d5bf7018234ad4e1c813de459354
QAISER ALI <qaisers.official@gmail.com>
2024-09-26 11:58:58 +0500

    Refactor array initialization without any specific size

--- Arrays/All About Arrays.cpp
+++ Arrays/All About Arrays.cpp
@@ -6,6 +6,10 @@ int main() {
     // int arr[5];
 
     // Array Initialization in C++
-    int arr[5] = {1, 2, 3, 4, 5};
+    // int arr[5] = {1, 2, 3, 4, 5};
+
+    // Array Initialization in C++ (Without Specifying Size)
+    int arr[] = {1, 2, 3, 4, 5}; //the length of an array is equal to the number of elements inside curly braces.
+    cout << arr[0] << endl;
 return 0;
 }
