851fa4c45ed21182df8226973d0c5aef4b545f42
QAISER ALI <qaisers.official@gmail.com>
2024-09-26 11:54:09 +0500

    Refactor array declaration and initialization in C++

--- Arrays/All About Arrays.cpp
+++ Arrays/All About Arrays.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main() {
     // Array Decaration in C++
-    int arr[5];
+    // int arr[5];
+
+    // Array Initialization in C++
+    int arr[5] = {1, 2, 3, 4, 5};
 return 0;
 }
