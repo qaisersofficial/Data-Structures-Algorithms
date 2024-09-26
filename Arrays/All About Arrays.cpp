47b1c447dfe8e35577432b8f979ded9ff032e28e
QAISER ALI <qaisers.official@gmail.com>
2024-09-26 12:15:45 +0500

    Refactor array initialization and add dynamic array declaration and input in C++

--- Arrays/All About Arrays.cpp
+++ Arrays/All About Arrays.cpp
@@ -14,12 +14,26 @@ int main() {
 
 
     // initlize array after decaration (using loop)
-    int arr[5];
-    for (int i = 0; i < 5; i++) {
-        arr[i] = i + 1; // we can also take input from the user using cin >> arr[i];
-    }// this method is used when we want to take input from the user or we cant to asssign elements one by one to each index of the array.
-    for (int i = 0; i < 5; i++) {
-        cout << arr[i] << " "; // output: 1 2 3 4 5
-    }
+    // int arr[5];
+    // for (int i = 0; i < 5; i++) {
+    //     arr[i] = i + 1; // we can also take input from the user using cin >> arr[i];
+    // }// this method is used when we want to take input from the user or we cant to asssign elements one by one to each index of the array.
+    // for (int i = 0; i < 5; i++) {
+    //     cout << arr[i] << " "; // output: 1 2 3 4 5
+    // }
+
+    // getting input from user and print the array
+    int N;
+    cout<<"Please tell me how many elements you want to enter in the array: ";
+    cin >> N;
+        int arr[N]; // this is called dynamic array
+        cout<< "Type the element of the array:";
+        for( int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cout << " The elements of the array are: ";
+        for ( int i = 0; i < N; i++){
+            cout << arr[i] << " ";
+        }
 return 0;
 }
