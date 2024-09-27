94613aab82fb23acef48b97b1f167c368f4ae01e
QAISER ALI <qaisers.official@gmail.com>
2024-09-27 16:21:55 +0500

    Refactor code to initialize head pointer in singlyLinkList constructor

--- Singly LinkList/Insertion operations/Insertion in LinkList.cpp
+++ Singly LinkList/Insertion operations/Insertion in LinkList.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+struct Node // Node structure
+{
+    int data;   // Data to store
+    Node* next; // Pointer to next node
+};
+class singlyLinkList
+{
+    public:
+    Node* head; // Head of the link list
+    singlyLinkList() // Constructor
+    {
+        head = NULL; // Initialize head to NULL
+    }
+};
 int main() {
-     
+    singlyLinkList sLL; // Object of singlyLinkList
 return 0;
 }
