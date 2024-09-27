b63065a02c87278c1419a6992481a618542be47b
QAISER ALI <qaisers.official@gmail.com>
2024-09-27 16:33:22 +0500

    created a insertion function that insert the value at start of the every linklist when it calls and then display the result with the help of tempurary pointer

--- Singly LinkList/Insertion operations/Insertion in LinkList.cpp
+++ Singly LinkList/Insertion operations/Insertion in LinkList.cpp
@@ -12,10 +12,30 @@ class singlyLinkList
     Node* head; // Head of the link list
     singlyLinkList() // Constructor
     {
-        head = NULL; // Initialize head to NULL
+        head = nullptr; // Initialize head to NULL, we can also use nullptr 
+    }
+    void insertAtBegin(int data) // Function to insert at the beginning
+    {
+        Node* newNode = new Node(); // Create a new node
+        newNode->data = data; // Assign data to the new node
+        newNode->next = head; // Point the new node to the head
+        head = newNode; // Make the new node as head
+    }
+    void display() // Function to display the link list
+    {
+        Node* temp = head; // Create a temporary pointer to head
+        while(temp != nullptr) // Iterate till the end of the link list
+        {
+            cout << temp->data << " "; // Print the data
+            temp = temp->next; // Move the pointer to the next node
+        }
     }
 };
 int main() {
     singlyLinkList sLL; // Object of singlyLinkList
+    sLL.insertAtBegin(10); // Insert 10 at the beginning
+    sLL.insertAtBegin(20); // Insert 20 at the beginning
+    sLL.insertAtBegin(30); // Insert 30 at the beginning
+    sLL.display(); // Display the singly link list
 return 0;
 }
