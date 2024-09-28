699794abfe0f42020c3035f014a70dcbb5ff1a74
QAISER ALI <qaisers.official@gmail.com>
2024-09-28 22:17:07 +0500

    delete a node from front done and now i am going to write a insertion function and then i will test to delete nodes to test it again

--- Singly LinkList/Deletion Operations/Deletion in Singly LinkList.cpp
+++ Singly LinkList/Deletion Operations/Deletion in Singly LinkList.cpp
@@ -14,9 +14,21 @@ class singlyLinkList
     {
         head = nullptr; // Initialize head to NULL, we can also use nullptr 
     }
+
+   // Delete a node from the beginning.
+        void deleteFromBeginning(){
+            if (head == nullptr) {
+                cout << "List is empty, nothing to delete." << endl;
+                return;
+            }
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            cout << "Node deleted from the beginning." << endl;
+        }
 };
 int main() {
     singlyLinkList sLL; // Object of singlyLinkList
-    
+    sLL.deleteFromBeginning(); // output: List is empty, nothing to delete.
     return 0;
 }
